a9067bb4223c93bcde3b421f8bdc0eef52813db7
Matt Raible <matt@raibledesigns.com>
2024-05-22 23:16:02 -0600

    Updates after QA

--- tests/ab.sh
+++ tests/ab.sh
@@ -1,25 +1,26 @@
 # -c is concurrent clients
 # -n is the number of requests to run in the test
 # -k is keep alive i.e., perform multiple requests within one HTTP session
-# ulimit -n 10000
-ab -H Authorization:"Bearer $TOKEN" -c 5000 -k -n 10000 $URL/hello
+# -s is timeout (default is 30 seconds)
+ulimit -n 10000
+ab -H Authorization:"Bearer $TOKEN" -c 1000 -k -n 5000 -s 120 $URL/hello
 
-# Gitpod: 4 cores, 8 GB RAM
+# Gitpod Standard: 4 cores, 8 GB RAM
 
 # http $URL/hello Authorization:"Bearer $TOKEN"
 
 # ==========================================
 # Imperative Results
 # ==========================================
-# Micronaut: 2 failed, 106.22 requests per second, 9.414 [ms] time per request
-# Quarkus: 2 failed, 104.09 rps, 9.607 tpr
-# Spring Boot: 0 failed, 101.21 rps, 9.880 tpr
-# Helidon: Total of 7725 requests completed
+# Micronaut: 66.28 requests per second, 15.087 [ms] time per request, 75.435 [s] time for tests
+# Quarkus: 126.93 rps, 7.879 tpr, 39.393
+# Spring Boot: 125.4 rps, 7.972 tpr, 39.860
+# Helidon: 367.95 rps, 2.718 tpr, 13.589
 
 # ==========================================
 # Reactive Results
 # ==========================================
-# Micronaut: Total of 2145 requests completed
-# Quarkus: Total of 6015 requests completed
-# Spring Boot: Total of 9173 requests completed
-# Helidon: 0 failed, 106.07 rps, 9.427 [ms] time per request
+# Micronaut: 69.10 rps, 14.471 tpr, 72.354
+# Quarkus: 218.88, 4.569, 22.843
+# Spring Boot: 69.02, 14.488, 72.442
+# Helidon: 70.29, 14.226, 71.131
